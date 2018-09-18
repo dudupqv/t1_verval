c2bafd937c39cd6d2a55f824c01d1cf7bd0227ae
lis <80106615@pucrs.br>
2018-09-18 10:23:28 -0300

    GameEntryTest

--- ClassesParaAvaliacao/GameEntryTest.java
+++ ClassesParaAvaliacao/GameEntryTest.java
@@ -1,27 +1,42 @@
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
 class GameEntryTest {
-
-	@Test
-	void testGameEntry() {
-		fail("Not yet implemented");
-	}
+	
+	String name = "Eduardo";
+	int score = 0;
+	String outTest = "("+name+", "+score+")";
+	GameEntry ge1 = new GameEntry(name, score);
 
 	@Test
 	void testGetName() {
-		fail("Not yet implemented");
+		assertFalse("String é nula", ge1.getName().equals(null));
+		assertEquals(name, ge1.getName());
+	}
+	
+	@Test
+	boolean testScoreValue(int value) {
+		if(value >= 0 && value <= Integer.MAX_VALUE){
+			return true;
+		}
+		return false;
 	}
 
 	@Test
 	void testGetScore() {
-		fail("Not yet implemented");
+		assertTrue("Score nao está entre 0 e Integer(MAX_VALUE)", testScoreValue((ge1.getScore())));
+		assertFalse("Teste limite inferior -1",-1 == ge1.getScore());
+		assertTrue("Teste limite inferior 0, 1",ge1.getScore() <= 0);		
+		assertFalse("Teste limite superior Interger.MAX_VALUE", Integer.MAX_VALUE+1 == ge1.getScore());
+		assertTrue("Teste limite superior Interger.MAX_VALUE -1, Interger.MAX_VALUE",Integer.MAX_VALUE >= ge1.getScore());
 	}
 
 	@Test
 	void testToString() {
-		fail("Not yet implemented");
+		assertEquals(outTest, ge1.toString());
 	}
 
 }
