197987393965e58d3fd9d6630143d018513c3a83
GabrielFranzoni <gabriel.franzoni@pucrs.br>
2018-09-18 17:00:07 -0300

    vai se fuder dudu

--- ClassesParaAvaliacao/ScoresBuggedImplTest.java
+++ ClassesParaAvaliacao/ScoresBuggedImplTest.java
@@ -1,81 +1,114 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ScoresBuggedImplTest {
-	ScoresBuggedImpl scores = new ScoresBuggedImpl();
-	String testeToString = "[" + "(" + "Gabriel" + ", " + "1" + "), " +
-						         "(" + "Felipe" + ", " + "0" + ")" +"]";
-
-	@Test
-	void testAddAndRemove() {
-		
-		//score menor que 0
-		Assertions.assertThrows(IllegalArgumentException.class, () -> scores.add(new GameEntry("Eduardo", -1)));
-		//score maior que o limite
-		Assertions.assertThrows(IllegalArgumentException.class, () -> scores.add(new GameEntry("Gael", (Integer.MAX_VALUE+1))));
-		
-		//score igual ao limite inferior
-		assertTrue(scores.add(new GameEntry("Felipe", 0)));
-		//score igual ao limite inferior + 1
-		assertTrue(scores.add(new GameEntry("Gabriel", 1)));
-		
-		
-		//teste ToString
-		assertEquals(testeToString, scores.toString());
-		
-		
-		//score igual ao limite superior - 1
-		assertTrue(scores.add(new GameEntry("Leonardo", (Integer.MAX_VALUE-1))));
-		//score igual ao limite superior
-		assertTrue(scores.add(new GameEntry("Rafael", (Integer.MAX_VALUE))));
-		
-		
-		assertTrue(scores.add(new GameEntry("Fernando", 381)));
-		//Tentando adicionar alguem com vetor full
-		assertTrue(scores.add(new GameEntry("João", 400)));
-		
-		// removendo e verificando que a primeira posição do vetor
-		// posui a pessoa com o maior score
-		assertEquals("Rafael", scores.remove(0).getName());
-		//Após a primeira remoção a primeira posição é atualizada
-		//E desse jeito o primeiro lugar fica o segundo melhor score
-		assertEquals("Leonardo", scores.remove(0).getName());
-		assertEquals("João", scores.remove(0).getName());
-		assertEquals("Fernando", scores.remove(0).getName());
-		assertEquals("Gabriel", scores.remove(0).getName());
-		
-	}
-
-	@Test
-	void testGetNumElements() {
-		assertTrue(scores.getNumElements() >= 0);
+	
+	ScoresBuggedImpl scores;
+	String testeToString;
+	
+	// Construtor de testes
+	@BeforeEach
+	void setup() {
+		scores = new ScoresBuggedImpl();
+		testeToString = "[" + "(" + "Gabriel" + ", " + "10" + "), " +
+					         "(" + "Eduardo" + ", " + "5" + ")" +"]";
+		scores.add(new GameEntry("Eduardo", 5));
+		scores.add(new GameEntry("Gabriel", 10));
 	}
 	
+	// Teste realizado em cima dos getters Capacity e NumElements
+	// Ambos seguem a regra das tabelas mostradas no relatório
 	@Test
-	public final void testeRemove2(){
-		Assertions.assertThrows(IndexOutOfBoundsException.class, () -> scores.remove(-1));
-    }
-	
-	@Test
-	public final void testeRemove3(){
-		Assertions.assertThrows(IndexOutOfBoundsException.class, () -> scores.remove(1));
-    }
-	
-	@Test
-	public final void testeRemove4(){
-		Assertions.assertThrows(IndexOutOfBoundsException.class, () -> scores.remove(5));
-    }
-	
-	@Test
-	public final void testeAdd(){
-		Assertions.assertThrows(NullPointerException.class, () -> scores.add(null));
-    }
-	
-	@Test
-	void testGetCapacity() {
+	void gettersTest() {
+		assertEquals(2, scores.getNumElements());
 		assertEquals(5, scores.getCapacity());
+		assertTrue(scores.getNumElements() >= 0);
+		assertTrue(scores.getCapacity() == 5);
+	}
+	
+	// Teste em cima do método toString(), para garantir que retorne o formato da String proposto
+	@Test
+	void toStringTest() {
+		//teste do método ToString
+		assertEquals(testeToString, scores.toString());
+	}
+
+	// A seguir, uma série de testes realizados para garantir que o método add funciona
+	// O método a seguir realiza testes com os limites (apresentados na tabela de casos de teste)
+	@Test
+	void addTest() {
+		//Score menor que 0 (limite inferior)
+		Assertions.assertThrows(IllegalArgumentException.class, () -> scores.add(new GameEntry("Eduardo", -1)));
+		//Score maior que o limite (limite superior)
+		Assertions.assertThrows(IllegalArgumentException.class, () -> scores.add(new GameEntry("Gael", (Integer.MAX_VALUE+1))));
+		//Score igual ao limite inferior
+		assertTrue(scores.add(new GameEntry("Felipe", 0)));
+		//Score igual ao limite inferior + 1
+		assertTrue(scores.add(new GameEntry("Gabriel", 1)));
+		//Score igual ao limite superior - 1
+		assertTrue(scores.add(new GameEntry("Leonardo", (Integer.MAX_VALUE-1))));
+		//Score igual ao limite superior
+		assertTrue(scores.add(new GameEntry("Rafael", (Integer.MAX_VALUE))));
+	}
+	
+	// A seguir, são testes que garantem ele insere Scores no vetor quando são maiores e não insere quando são menores
+	@Test
+	void fullTest() {
+		// Lota o vetor
+		assertTrue(scores.add(new GameEntry("Ijuí", 15)));
+		assertTrue(scores.add(new GameEntry("Maurício", 20)));
+		assertTrue(scores.add(new GameEntry("Gesiel", 25)));
+		
+		// Insere novo score no vetor, sucedendo pois ele é maior que o menor atual (que é Eduardo,5)
+		assertTrue(scores.add(new GameEntry("Casarão", 40)));
+		
+		// Falha ao inserir novo Score por ele ser menor que o menor score no vetor
+		assertFalse(scores.add(new GameEntry("Fracassado", 1)));
+	}
+	
+	// A seguir, estão os testes de remoção do vetor
+	// O primeiro teste é realizado como segue a tabela de casos teste
+	@Test
+	void removeTest() {
+		// Lota o vetor
+		scores.add(new GameEntry("Ijuí", 15));
+		scores.add(new GameEntry("Maurício", 20));
+		scores.add(new GameEntry("Gesiel", 25));
+		
+		// Posição inferior ao limite inferior
+		Assertions.assertThrows(IndexOutOfBoundsException.class, () -> scores.remove(-1));
+		// Posição superior ao limite superior
+		Assertions.assertThrows(IndexOutOfBoundsException.class, () -> scores.remove(5));
+		// Posição final do limite superior
+		assertNotNull(scores.remove(4));
+		// Posição final do limite inferior
+		assertNotNull(scores.remove(0));
+		
+		// O próximo teste apenas garante que ele atire uma exceção, caso na posição desejada não esteja nenhum score
+		// Ou seja, um null
+		Assertions.assertThrows(IndexOutOfBoundsException.class, () -> scores.remove(4));
+	}
+	
+	
+	// Este teste final garante que ele mantenha a ordem do vetor. Ele deve remover em ordem de menor para maior:
+	// Casarão, Gesiel, Maurício, Ijuí e Gabriel. Se o teste suceder, significa que o vetor está ordenado de acordo.
+	@Test
+	void testOrder() {
+		// Lota o vetor
+		scores.add(new GameEntry("Ijuí", 15));
+		scores.add(new GameEntry("Maurício", 20));
+		scores.add(new GameEntry("Gesiel", 25));
+		// Substitui
+		scores.add(new GameEntry("Casarão", 40));
+		
+		assertTrue(scores.remove(4).getName().equals("Gabriel"));
+		assertTrue(scores.remove(3).getName().equals("Ijuí"));
+		assertTrue(scores.remove(2).getName().equals("Maurício"));
+		assertTrue(scores.remove(1).getName().equals("Gesiel"));
+		assertTrue(scores.remove(0).getName().equals("Casarão"));
 	}
 
 }
